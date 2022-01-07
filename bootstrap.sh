478479eb5040598c4e45ab75fe39990387a56387
Oliver Gould <ver@buoyant.io>
2022-01-07 19:55:43 +0000

    add less

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 DOTFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 
-sudo apt install -y vim zsh
+sudo apt install -y less vim zsh
 
 curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh
 cp $DOTFILE_DIR/.zshrc $HOME/.zshrc
