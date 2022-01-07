1577b676b1b9bb218d25ca728940c947e4c2e2cf
Oliver Gould <ver@buoyant.io>
2022-01-07 19:03:06 +0000

    install zshrc properly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+DOTFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 
 sudo apt install -y vim zsh
 
 curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh
-cp .zshrc ~/.zshrc
+cp $DOTFILE_DIR/.zshrc $HOME/.zshrc
