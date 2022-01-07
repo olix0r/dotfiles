01716bec14977f69cd7e10a4486abe58d9992388
Oliver Gould <ver@buoyant.io>
2022-01-07 18:58:41 +0000

    install zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,5 @@
 
 sudo apt install -y vim zsh
 
-# TODO oh my zsh
-# TODO install zshrc
-
-env > ~/.configured
-
-#sudo chsh -s /usr/bin/zsh $(whoami)
 curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh
-
-# export EDITOR=vim
-# export VISUAL=$EDITOR
+cp .zshrc ~/.zshrc
