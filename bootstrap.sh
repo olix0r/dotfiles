49bae54e75147058c6554d171e03f31e7ae6fc1f
Oliver Gould <ver@buoyant.io>
2022-01-07 18:38:11 +0000

    Try changing the shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,10 @@ sudo apt install -y vim zsh
 
 # TODO oh my zsh
 # TODO install zshrc
-# TODO set git config
 
-export SHELL=/usr/bin/zsh
-export EDITOR=vim
-export VISUAL=$EDITOR
+env > ~/.configured
+
+sudo chsh -s /usr/bin/zsh $(whoami)
+
+# export EDITOR=vim
+# export VISUAL=$EDITOR
