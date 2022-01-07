b51a3f6c588650ef56601d16547eca150f74a9b4
Oliver Gould <ver@buoyant.io>
2022-01-07 21:47:48 +0000

    more utils

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,13 @@
 
 DOTFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 
-sudo apt install -y less vim zsh
+sudo apt install -y htop less psmisc vim zsh
+
+mkdir -p $HOME/bin
 
 curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh
 cp $DOTFILE_DIR/.zshrc $HOME/.zshrc
 
 cp $DOTFILE_DIR/.gitignore $HOME/.gitignore
 
-cp -rp $DOTFILE_DIR/bin $HOME/bin
+cp -rp $DOTFILE_DIR/bin/* $HOME/bin/
