e7c3df49ca33113355d128ea9c100e734b2db862
Oliver Gould <ver@buoyant.io>
2022-02-05 18:52:51 +0000

    Set prompt colors per session; add jo

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ DOTFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 sudo apt install -y \
     gnupg2 \
     htop \
+    jo \
+    jq \
     less \
     locales \
     psmisc \
@@ -17,14 +19,21 @@ sudo apt install -y \
 sudo sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
 sudo locale-gen
 
-mkdir -p $HOME/bin
-
-cp $DOTFILE_DIR/.zshenv $HOME/.zshenv
-curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh
-cp $DOTFILE_DIR/.zshrc $HOME/.zshrc
-
 cp $DOTFILE_DIR/.gitignore $HOME/.gitignore
 
+curl --proto '=https' --tlsv1.3 -vfsSL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh
+cp $DOTFILE_DIR/.zshenv $HOME/.zshenv
+(
+    echo
+    echo "export PROMPT_PREFIX_COLOR=$((RANDOM % 190 + 40))"
+    echo "export PROMPT_DIR_COLOR=$((RANDOM % 190 + 40))"
+    echo "export PROMPT_GIT_COLOR=$((RANDOM % 190 + 40))"
+) >>$HOME/.zshenv
+cp $DOTFILE_DIR/.zshrc $HOME/.zshrc
+
+mkdir -p $HOME/bin
 cp -rp $DOTFILE_DIR/bin/* $HOME/bin/
 
-k3d kubeconfig merge -d 2>/dev/null || true
+if command -v k3d >/dev/null 2>&1 ; then
+    k3d kubeconfig merge -d 2>/dev/null || true
+fi
