76f52dbdb756dd33e7f8a987193cfa1aac87074c
Oliver Gould <ver@buoyant.io>
2022-05-28 04:20:22 +0000

    try to fixup just plugin
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- omz/plugins/just/just.plugin.zsh
+++ omz/plugins/just/just.plugin.zsh
@@ -1,6 +1,9 @@
-#compdef just
+#compdef __just just
 
 if (( $+commands[just] )); then
     just --completions=zsh > "$ZSH_CACHE_DIR/completions/_just"
-    source "$ZSH_CACHE_DIR/completions/_just"
 fi
+
+__just() {
+    source "$ZSH_CACHE_DIR/completions/_just"
+}
