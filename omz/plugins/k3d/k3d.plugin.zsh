df9dafc049fcf33373bed5bce350b4d3fb935e42
Oliver Gould <ver@buoyant.io>
2022-05-28 05:37:01 +0000

    Tidy up completions

--- omz/plugins/k3d/k3d.plugin.zsh
+++ omz/plugins/k3d/k3d.plugin.zsh
@@ -1,4 +1,11 @@
-if (( $+commands[k3d] )); then
-    k3d completion zsh > "$ZSH_CACHE_DIR/completions/_k3d"
-    source "$ZSH_CACHE_DIR/completions/_k3d"
+(( $+commands[k3d] ))|| return
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `k3d`. Otherwise, compinit will have already done that
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_k3d" ]]; then
+    autoload -Uz _k3d
+    typeset -g -A _comps
+     _comps[k3d]=_k3d
 fi
+
+k3d completion zsh > "$ZSH_CACHE_DIR/completions/_k3d" &!
--- omz/plugins/linkerd/linkerd.plugin.zsh
+++ omz/plugins/linkerd/linkerd.plugin.zsh
@@ -1,4 +1,11 @@
-if (( $+commands[linkerd] )); then
-    linkerd completion zsh > "$ZSH_CACHE_DIR/completions/_linkerd"
-    source "$ZSH_CACHE_DIR/completions/_linkerd"
+(( $+commands[linkerd] )) || return
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `linkerd`. Otherwise, compinit will have already done that
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_linkerd" ]]; then
+    autoload -Uz _linkerd
+    typeset -g -A _comps
+     _comps[linkerd]=_linkerd
 fi
+
+linkerd completion zsh >! "$ZSH_CACHE_DIR/completions/_linkerd" &!
