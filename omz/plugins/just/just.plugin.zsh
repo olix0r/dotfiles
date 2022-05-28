f5fb074aa8c50ed662e3fcbdffae2763ab56a9e7
Oliver Gould <ver@buoyant.io>
2022-05-28 05:01:56 +0000

    omz: Make the just plugin more like rust/cargo
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- omz/plugins/just/just.plugin.zsh
+++ omz/plugins/just/just.plugin.zsh
@@ -1,9 +1,11 @@
-#compdef __just just
+(( $+commands[just] )) || return
 
-if (( $+commands[just] )); then
-    just --completions=zsh > "$ZSH_CACHE_DIR/completions/_just"
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `just`. Otherwise, compinit will have already done that
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_just" ]]; then
+  autoload -Uz _just
+  typeset -g -A _comps
+  _comps[just]=_just
 fi
 
-__just() {
-    source "$ZSH_CACHE_DIR/completions/_just"
-}
+just --completions=zsh >! "$ZSH_CACHE_DIR/completions/_just" &!
